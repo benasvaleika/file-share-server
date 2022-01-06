2edbe7bf57d5ff4b5e0bc3a8dce20bbdd5a085c2
benasvaleika <bentini113@gmail.com>
2022-01-06 14:35:58 +0200

    Eslint

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Request, Response } from 'express';
+import http from 'http';
+import WebSocket from 'ws';
+import dotenv from 'dotenv';
+
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello express");
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server: server });
+
+wss.on('connection', (ws) => {
+  console.log('A new client connected');
+  ws.send('conn successful');
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello express');
 });
 
 app.listen(PORT, () => {
